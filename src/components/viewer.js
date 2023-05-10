a493adb25a38b91d30bb9671cb1554c9f037b956
James Truitt <jtruitt1@swarthmore.edu>
2023-05-10 12:12:59 -0400

    Extract service url from page-manifest

--- src/components/viewer.js
+++ src/components/viewer.js
@@ -11,9 +11,20 @@ const Viewer = () => {
   React.useEffect(() => {
     if (typeof window !== 'undefined' && typeof document !== 'undefined') {
       import('openseadragon').then(OpenSeaDragon => {
+
+        // Get manifest URL
+        pageManifestUrl = encodeURI("https://web.tricolib.brynmawr.edu/digitalcollections/node/334752/page-manifest")
+        const getPageManifest = async (url) => {
+          const response = await fetch(url)
+          let pageManifest = await response.json()
+          return pageManifest
+        }
+        pageManifest = getPageManifest(pageManifestUrl)
+        manifestUrl = pageManifest.sequences[0].canvases[0].images[0].resource.service['@id']
+
         // Set the tile sources.
         const tileSources = [
-          encodeURI("https://web.tricolib.brynmawr.edu/digitalcollections/node/334752/page-manifest")
+          encodeURI(manifestUrl)
         ];
         // Create the viewer.
         const viewer = new OpenSeaDragon.default({
