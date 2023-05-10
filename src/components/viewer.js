532acf365e9ec647b10428c3c86da5b18ac4a770
James Truitt <jtruitt1@swarthmore.edu>
2023-05-10 12:20:19 -0400

    Update viewer.js

--- src/components/viewer.js
+++ src/components/viewer.js
@@ -20,6 +20,7 @@ const Viewer = () => {
           return pageManifest
         };
         const pageManifest = getPageManifest(pageManifestUrl)
+        console.log(pageManifest.sequences[0])
         const manifestUrl = pageManifest.sequences[0].canvases[0].images[0].resource.service['@id']
 
         // Set the tile sources.
