f334ba9e36a3f0dbbc813c7b31e109654899867e
James Truitt <jtruitt1@swarthmore.edu>
2023-05-10 14:24:51 -0400

    Try not using the proxy and rebuild

--- src/components/viewer.js
+++ src/components/viewer.js
@@ -14,7 +14,7 @@ const Viewer = () => {
 
         // Set the tile sources.
         const tileSources = [
-          encodeURI("https://web.tricolib.brynmawr.edu/digitalcollections/cantaloupe/iiif/2/https%3A%2F%2Fdigitalcollections.tricolib.brynmawr.edu%2F_flysystem%2Ffedora%2F2023-03%2F1270_0.jp2")
+          encodeURI("https://digitalcollections.tricolib.brynmawr.edu/cantaloupe/iiif/2/https%3A%2F%2Fdigitalcollections.tricolib.brynmawr.edu%2F_flysystem%2Ffedora%2F2023-03%2F1270_0.jp2/info.json")
         ];
         // Create the viewer.
         const viewer = new OpenSeaDragon.default({
